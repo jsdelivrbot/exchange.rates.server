d9e1f34835ccb7dea95668009a318a2ea4e9ed7f
nullso <nullso.mail@gmail.com>
2017-12-17 10:56:55 +0300

    some fixes

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
@@ -13,9 +12,6 @@ app.engine('ejs', require('ejs-locals'));
 // view engine setup
 app.set('views', path.join(__dirname, 'templates'));
 app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 if (app.get('env') === 'development') app.use(logger('dev'));
 else app.use(logger('default'));
