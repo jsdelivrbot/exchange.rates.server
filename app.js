04f0909912801d9eb6dbfc0e3241d9532f2c0f03
nullso <nullso.mail@gmail.com>
2017-12-26 22:30:33 +0300

    fix index.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-//const index = require('routes/index');
+const index = require('./routes/index');
 const app = express();
 
 app.engine('ejs', require('ejs-locals'));
@@ -13,12 +13,6 @@ app.set('views', path.join(__dirname, 'templates'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
-
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'ejs');
-
 app.get('/', (req, res) => res.render('pages/index'));
-
-//  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 module.exports = app;
