ec371c1f7c45f1b4f82241451a229e793c13ab86
nullso <nullso.mail@gmail.com>
2017-12-26 22:32:40 +0300

    fix index.js

--- app.js
+++ app.js
@@ -12,7 +12,41 @@ app.engine('ejs', require('ejs-locals'));
 app.set('views', path.join(__dirname, 'templates'));
 app.set('view engine', 'ejs');
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+if (app.get('env') === 'development') app.use(logger('dev'));
+else app.use(logger('default'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', (req, res) => res.render('pages/index'));
+
+// routes
+app.use('/', index);
+//app.use('/users', users);
+
+//app.get('/', (req, res) => res.render('pages/index'));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next)
+{
+	let err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next)
+{
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
+});
 
 module.exports = app;
