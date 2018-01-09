955190aa9b65740e9d495b7323b604f62b723aea
nullso <nullso.mail@gmail.com>
2018-01-09 21:08:14 +0300

    fix getPages.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 const express = require('express');
 const router = express.Router();
 
+let data = 'NO Data';
+/**
+ * Get pages from source urls
+ */
+let renderSourcePages = () =>
+{
+	let page = new Promise(resolve => resolve(require('../bin/getPages.js')))
+		.then(result => data = JSON.stringify(result, "", 4))
+		.catch(reject => log.error("Rejected -> " + reject));
+};
+
+/**
+ * Cron job
+ */
+let CronJob = require('cron').CronJob;
+new CronJob({
+	//cronTime: "0 5,20,35,50 * * * *", // Every 15 minutes
+	//cronTime: "5,20,35,50 * * * * *", // Every 15 sec
+	cronTime: "30 * * * * *", // for upload
+	onTick: renderSourcePages,
+	start: true,
+	timeZone: "America/Los_Angeles"
+});
+
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res, next)
+{
+	res.render('index', {
+		title: '',
+		main: data
+	});
 });
 
 module.exports = router;
